b23506dc480497c6344d2f9ba63d977bcd7a7620
ironbeer <7997273+ironbeer@users.noreply.github.com>
2022-06-21 17:46:53 +0900

    - Updated L1StandardERC20Factory and L1StandardERC721Factory contract. - Improved deployment of built-in contracts.

--- contracts/oasys/token.go
+++ contracts/oasys/token.go
@@ -1,34 +1,12 @@
 package oasys
 
-import "math/big"
-
 var (
-	tokenContractSet = contractSet{
-		{
-			name:    "WOAS",
-			address: "0x5200000000000000000000000000000000000001",
-			code:    wrappedOASCode,
-			fixedStorage: map[string]interface{}{
-				// string public name
-				"0x00": "Wrapped OAS",
-				// string public symbol
-				"0x01": "WOAS",
-				// uint8 public decimals
-				"0x02": big.NewInt(18),
-			},
-		},
-		{
-			name:    "SOAS",
-			address: "0x5200000000000000000000000000000000000002",
-			code:    stakableOASCode,
-			fixedStorage: map[string]interface{}{
-				// string public name
-				"0x03": "Stakable OAS",
-				// string public symbol
-				"0x04": "SOAS",
-				// address public staking
-				"0x05": "0x0000000000000000000000000000000000001001",
-			},
-		},
+	wrappedOAS = &contract{
+		name:    "WOAS",
+		address: "0x5200000000000000000000000000000000000001",
+	}
+	stakableOAS = &contract{
+		name:    "SOAS",
+		address: "0x5200000000000000000000000000000000000002",
 	}
 )
--- contracts/oasys/tokenFactory.go
+++ contracts/oasys/tokenFactory.go
@@ -1,16 +1,12 @@
 package oasys
 
 var (
-	tokenFactoryContractSet = contractSet{
-		{
-			name:    "L1StandardERC20Factory",
-			address: "0x5200000000000000000000000000000000000004",
-			code:    l1StandardERC20FactoryCode,
-		},
-		{
-			name:    "L1StandardERC721Factory",
-			address: "0x5200000000000000000000000000000000000005",
-			code:    l1StandardERC721FactoryCode,
-		},
+	l1StandardERC20Factory = &contract{
+		name:    "L1StandardERC20Factory",
+		address: "0x5200000000000000000000000000000000000004",
+	}
+	l1StandardERC721Factory = &contract{
+		name:    "L1StandardERC721Factory",
+		address: "0x5200000000000000000000000000000000000005",
 	}
 )
