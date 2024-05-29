999617222c20cff22f8a8d29090854df41422d7f
tak <re795h@gmail.com>
2024-05-29 13:10:40 +0900

    update IsShanghai check function to activate shanhai fork besed on chain id

--- params/config.go
+++ params/config.go
@@ -609,7 +609,17 @@ func (c *ChainConfig) IsTerminalPoWBlock(parentTotalDiff *big.Int, totalDiff *bi
 
 // IsShanghai returns whether time is either equal to the Shanghai fork time or greater.
 func (c *ChainConfig) IsShanghai(num *big.Int, time uint64) bool {
-	return c.IsLondon(num) && isTimestampForked(c.ShanghaiTime, time)
+	if c.Oasys == nil {
+		return c.IsLondon(num) && isTimestampForked(c.ShanghaiTime, time)
+	}
+	shanghaiTime := newUint64(0) // 0 means that Shanghai is activated
+	if c.ChainID.Cmp(OasysMainnetChainConfig.ChainID) == 0 {
+		shanghaiTime = OasysMainnetChainConfig.ShanghaiTime
+	}
+	if c.ChainID.Cmp(OasysTestnetChainConfig.ChainID) == 0 {
+		shanghaiTime = OasysTestnetChainConfig.ShanghaiTime
+	}
+	return c.IsLondon(num) && isTimestampForked(shanghaiTime, time)
 }
 
 // IsCancun returns whether num is either equal to the Cancun fork time or greater.
