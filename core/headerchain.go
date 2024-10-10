2873b0df63bdea2db95f46087877060cf8744c8c
ironbeer <7997273+ironbeer@users.noreply.github.com>
2024-10-10 15:48:03 +0900

    Imported fast finality reorganization from bsc@v1.4.6

--- core/headerchain.go
+++ core/headerchain.go
@@ -306,7 +306,7 @@ func (hc *HeaderChain) writeHeadersAndSetHead(headers []*types.Header, forker *F
 		}
 	)
 	// Ask the fork choicer if the reorg is necessary
-	if reorg, err := forker.ReorgNeeded(hc.CurrentHeader(), lastHeader); err != nil {
+	if reorg, err := forker.ReorgNeededWithFastFinality(hc.CurrentHeader(), lastHeader); err != nil {
 		return nil, err
 	} else if !reorg {
 		if inserted != 0 {
