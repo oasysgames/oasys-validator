67eb53af32aba928443e31253a06cc7689355b83
tak <re795h@gmail.com>
2025-11-19 23:16:41 +0900

    Restore default gas limit to 30000000

--- miner/minerconfig/config.go
+++ miner/minerconfig/config.go
@@ -49,7 +49,7 @@ type Config struct {
 
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
-	GasCeil:  60_000_000, // Same as go-ethereum(v1.16.4)
+	GasCeil:  30000000,
 	GasPrice: big.NewInt(params.GWei),
 
 	// The default recommit time is chosen as two seconds since
