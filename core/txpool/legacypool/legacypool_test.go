1d2a42c7fe452fc38f5f04795454488a5201fdc6
tak <re795h@gmail.com>
2026-01-24 10:46:57 +0900

    fix test error

--- core/txpool/legacypool/legacypool_test.go
+++ core/txpool/legacypool/legacypool_test.go
@@ -56,6 +56,7 @@ var (
 func init() {
 	testTxPoolConfig = DefaultConfig
 	testTxPoolConfig.Journal = ""
+	testTxPoolConfig.NoLocals = false
 
 	cpy := *params.TestChainConfig
 	eip1559Config = &cpy
